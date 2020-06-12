c6a7173aa5cd9ab6e1033c450a61bd9d3e3b6786
ElbekDeveloper <e.normurodov@mail.ru>
2020-06-12 11:32:16 +0500

    Add drawing and erasing functionality

--- CanvasBoardUI/MainWindow.xaml.cs
+++ CanvasBoardUI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,54 @@ namespace CanvasBoardUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Brush CustomColor { get; set; }
+        public int CustomWidth { get; set; }
+        public int CustomHeight { get; set; }
+        Random r = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CanvasBoard_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Draw(object sender, MouseButtonEventArgs e)
+        {
+            
+                var leftBorder = Mouse.GetPosition(CanvasBoard).X - 25;
+                var topBorder = Mouse.GetPosition(CanvasBoard).Y -25;
+                CustomColor = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255)));
+                CustomWidth = r.Next(30, 50);
+                CustomWidth = r.Next(30, 50);
+
+                Rectangle newRect = new Rectangle
+                {
+                    Width = 50,
+                    Height = 50,
+                    Fill = CustomColor,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2
+                };
+
+                Canvas.SetLeft(newRect, leftBorder);
+                Canvas.SetTop(newRect, topBorder);
+
+                CanvasBoard.Children.Add(newRect);
+            
+
+        }
+
+        private void Erase(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Rectangle)
+            {
+                CanvasBoard.Children.Remove((Rectangle)e.OriginalSource);
+
+            }
+
         }
     }
 }
